c2a07f58472cc6b854fa2c8d46a4cfa9e16df6cc
Tyuiu.SokolovaAA@outlook.com <nas.sok@mail.ru>
2023-11-04 16:31:42 +0500

    Task1

---
+++ Tyuiu.SokolovaAA.Sprint4.Task1.V13/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SokolovaAA.Sprint4.Task1.V13.Lib;
+namespace Tyuiu.SokolovaAA.Sprint4.Task1.V13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService dataService = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнила: Cоколова A.A. | АСОиУб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Обработка структурных типов                                       *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнила: Cоколова Анастасия Александровна | АСОиУб-23-3                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный         *");
+            Console.WriteLine("* значениями с клавиатуры в диапазоне от 0 до 8 подсчитать сумму четных   *");
+            Console.WriteLine("* элементов массива.  С клавиатуры: 8, 1, 5, 4, 2, 1, 2, 7, 6, 1          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите длину массива: ");
+            int len = Convert.ToInt32(Console.ReadLine());
+
+            int[] numsArray = new int[len];
+
+            for (int i = 0; i < len; i++)
+            {
+                Console.Write("Введите значение {0} элемента массива: ", i);
+                numsArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Массив: {");
+            for (int i = 0; i < numsArray.Length; i++)
+            {
+                Console.Write(numsArray[i]);
+                if (i != numsArray.Length - 1) { Console.Write(", "); }
+            }
+            Console.WriteLine("}");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = dataService.Calculate(numsArray);
+
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
