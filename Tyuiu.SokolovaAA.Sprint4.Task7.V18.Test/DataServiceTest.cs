1f7c458113662f643c5bf8fe7514b830bf4c9876
Tyuiu.SokolovaAA@outlook.com <nas.sok@mail.ru>
2023-11-11 16:30:18 +0500

    Task7

---
+++ Tyuiu.SokolovaAA.Sprint4.Task7.V18.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SokolovaAA.Sprint4.Task7.V18.Lib;
+namespace Tyuiu.SokolovaAA.Sprint4.Task7.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void VaidCalc()
+        {
+            DataService ds = new DataService();
+            int rows = 5;
+            int columns = 3;
+            int[,] mtrx = new int[rows, columns];
+            string str = "145258749635789";
+            int res = ds.Calculate(rows, columns, str);
+            int wait = 12288;
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
