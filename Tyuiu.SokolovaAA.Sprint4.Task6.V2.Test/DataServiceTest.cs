6e0d92d3e1176479c46cf9e076eca2220a61fdd9
Tyuiu.SokolovaAA@outlook.com <nas.sok@mail.ru>
2023-11-11 16:19:14 +0500

    Task6

---
+++ Tyuiu.SokolovaAA.Sprint4.Task6.V2.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SokolovaAA.Sprint4.Task6.V2.Lib;
+namespace Tyuiu.SokolovaAA.Sprint4.Task6.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            var colors = new string[] { "Белый", "Черный", "Зеленый", "Синий", "Красный", "Желтый", "Фиолетовый" };
+            string[] res = ds.Calculate(colors);
+            string[] wait = { "Черный", "Зеленый", "Красный", "Желтый", "Фиолетовый" };
+            CollectionAssert.AreEqual( wait, res);
+        }
+    }
+}
